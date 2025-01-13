553bb21644185f7414317e88b1e7a65e1e5e94a7
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-01-13 19:36:36 +0100

    finished touches but still got some checks to do !

--- programs/lending_protocol/src/lib.rs
+++ programs/lending_protocol/src/lib.rs
@@ -1,14 +1,12 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::Token;
+use anchor_lang::system_program;
+use anchor_spl::token;
 
 declare_id!("79AoAxGPczqpkzg5ygo7rthSTh4DA7UZgFjyHocSPT2f");
 
 #[program]
 pub mod lending_protocol {
-
-
-    use anchor_lang::system_program;
-    use anchor_spl::token;
 
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -19,6 +17,8 @@ pub mod lending_protocol {
     pub fn lend_money(ctx : Context<Lend> , amount : u64) -> Result<()> {
         let lending_acc = &mut ctx.accounts.lending_acc;
         lending_acc.amount = amount;
+        
+
         system_program::transfer(
             CpiContext::new(
                 ctx.accounts.system_program.to_account_info(),
@@ -30,7 +30,6 @@ pub mod lending_protocol {
             lending_acc.amount
         )?;
         
-
         token::transfer(
             CpiContext::new_with_signer(
                 ctx.accounts.token_program.to_account_info(),
@@ -46,8 +45,47 @@ pub mod lending_protocol {
         Ok(())
     }
     
-    pub fn payment(ctx : Context<Lend> , amount : u64 , interest : u64) -> Result<()>{
+    pub fn payment(ctx : Context<Lend> , amount : u64 , interest_rate : u64) -> Result<()>{
+        let lending_acc = &mut ctx.accounts.lending_acc;
         
+        lending_acc.amount = amount;
+        lending_acc.interest_rate = interest_rate;
+        let client_balance = ctx.accounts.client.to_account_info();
+        
+        if client_balance.lamports() < lending_acc.amount / 5{
+            system_program::transfer(
+                CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer{
+                        from : ctx.accounts.client.to_account_info(),
+                        to : ctx.accounts.lender.to_account_info(),
+                    }
+                ),
+                client_balance.lamports()
+            )?;
+        }
+        
+        if lending_acc.amount <= 5 && lending_acc.amount >= 1 {
+            lending_acc.interest_rate = 2 / 100;
+        }
+        
+        if lending_acc.amount <= 20 && lending_acc.amount >= 11 {
+            lending_acc.interest_rate = 10 / 100;
+        }
+
+        lending_acc.payment_interest = lending_acc.amount + lending_acc.amount * lending_acc.interest_rate / 100;
+
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer{
+                        from : ctx.accounts.client.to_account_info(),
+                        to : ctx.accounts.lender.to_account_info(),
+                    }
+                ),
+                lending_acc.payment_interest
+            )?;
+
         Ok(())
     }
 
@@ -85,6 +123,7 @@ pub struct LendingData{
     pub token_account : Pubkey,
     pub amount : u64,
     pub interest_rate : u64,
+    pub payment_interest : u64
 }
 
 
